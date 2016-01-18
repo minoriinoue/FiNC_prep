7d0109b45ebadbcef95eef12537a439de21032a7
Minori Inoue <minoue1204@gmail.com>
2016-01-18 16:21:57 +0900

    changed the return value to an array from an integer.

--- ruby_basic/no11.rb
+++ ruby_basic/no11.rb
@@ -1,10 +1,15 @@
 def below_15(array)
+	result_array = []
 	sum = 0
 	array.each do |number|
 		sum += number
+		result_array.push(number)
 		if sum > 15
 			break
 		end
 	end
-	return sum
+	return result_array
 end
+
+array = [9, 4, 5, 2]
+puts "[9, 4, 5] = " + below_15(array).to_s
