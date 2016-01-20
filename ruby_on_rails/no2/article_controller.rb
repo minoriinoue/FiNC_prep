882c708e4e1449ce3cb75e3160884604eeeb3f9d
Minori Inoue <minori.inoue.1204@gmail.com>
2016-01-20 10:37:52 +0900

    deleted unnecessary path

--- ruby_on_rails/no2/article_controller.rb
+++
@@ -1,8 +0,0 @@
-class ArticlesController < ApplicationController
-  def show
-  @user = current_user
-  @article_title = Article.where(user_id: @user.id).first.title
-  @article_text = Article.where(user_id: @user.id).first.text
-  @comments = Comment.where(article_id: Aricle.where(user_id: @user.id))
-  end
-end
