fd157a63e121355855e8265ae1e9fcb9c27cf392
Minori Inoue <minori.inoue.1204@gmail.com>
2016-01-20 11:05:57 +0900

    Added answers for DRY

---
+++ ruby_on_rails/DRY/no2.rb
@@ -0,0 +1,40 @@
+class BookController < ApplicationController
+
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
+  
+  def find_book
+    @book = Book.find(params[:id])
+  end
+  
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(params[:book].permit(:title, :author))
+    book.save
+    redirect_to root_path
+  end
+
+  def edit
+    @book
+  end
+
+  def update
+    book.update(params[:book].permit(:title, :author))
+    redirect_to root_path
+  end
+
+  def destroy
+    book.destroy
+    redirect_to root_path
+  end
+end
