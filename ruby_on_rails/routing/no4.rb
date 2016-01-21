71074a299ee560772c1de5dedd94cfebb1cf8612
Minori Inoue <minori.inoue.1204@gmail.com>
2016-01-21 17:59:01 +0900

    Modified answers for no4

--- ruby_on_rails/routing/no4.rb
+++ ruby_on_rails/routing/no4.rb
@@ -1,19 +1,12 @@
 Rails.application.routes.draw do
-  namespace :admit do
-    resources :companies do
-      collection do
-        post 'create'
-      end
-
-      member do
-        collection do
-          get 'new'
-        end
-        
-        put 'edit'
-        delete 'destroy'
-
-      end
-    end
+  namespace :admin do
+    resources :companies, only: [:new, :create, :edit, :destroy]
   end
 end
+
+# result
+# Prefix             Verb   URI Pattern                         Controller#Action
+# admin_companies    POST   /admin/companies(.:format)          admin/companies#create
+# new_admin_company  GET    /admin/companies/new(.:format)      admin/companies#new
+# edit_admin_company GET    /admin/companies/:id/edit(.:format) admin/companies#edit
+# admin_company      DELETE /admin/companies/:id(.:format)      admin/companies#destroy
